c807bd926b5dc995e8b4984f234681d45df61c25
Daria Efimova <agaralambinia@yandex.ru>
2025-03-15 10:57:01 +0400

    wip

--- project/srcs/requirements/mariadb/conf/create_db.sh
+++ project/srcs/requirements/mariadb/conf/create_db.sh
@@ -2,9 +2,11 @@
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     chown -R mysql:mysql /var/lib/mysql
-
-    # Инициализация базы данных MariaDB
     mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+    tfile=`mktemp`
+    if [ ! -f "$tfile" ]; then
+        return 1
+    fi
 fi
 
 if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
@@ -16,9 +18,9 @@ DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
-CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS ${DB_NAME} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
-GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
     /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
